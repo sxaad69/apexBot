4778f0b23bd72ee45cc3c8f04232864764e7542c
sxaad69 <xsaad69@gmail.com>
2026-08-15 18:55:40 +0300

    fix: make STRATEGY_A7/A8_ENABLED env-overridable (default false for mainnet)

--- config/config.py
+++ config/config.py
@@ -212,8 +212,8 @@ class Config:
         self.STRATEGY_A4_ENABLED = self._str_to_bool('false')
         self.STRATEGY_A5_ENABLED = self._str_to_bool('false')
         self.STRATEGY_A6_ENABLED = self._str_to_bool('true')
-        self.STRATEGY_A7_ENABLED = self._str_to_bool('false')
-        self.STRATEGY_A8_ENABLED = self._str_to_bool('false')
+        self.STRATEGY_A7_ENABLED = self._str_to_bool(os.getenv('STRATEGY_A7_ENABLED', 'false'))
+        self.STRATEGY_A8_ENABLED = self._str_to_bool(os.getenv('STRATEGY_A8_ENABLED', 'false'))
         self.A8_IGNITION_THRESHOLD = float(os.getenv('A8_IGNITION_THRESHOLD', '0.40'))
         self.A6_ALLOW_SHORT = self._str_to_bool('false')
         
